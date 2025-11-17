3056fd870e6f353c1e3dd2f3a513fe805ab55c99
DonovanDebenPXL <donovan.deben@student.pxl.be>
2025-11-17 11:26:34 +0100

    Files toegevoegd

---
+++ PrintStudents/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PrintStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[2];
+            students[0] = new Student { FirstName = "Donovan", LastName = "Deben" };
+            students[1] = new Student { FirstName = "Jane", LastName = "Doe" };
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+        }
+    }
+}
