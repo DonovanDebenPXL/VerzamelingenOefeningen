3a4dac50fce6b94e7e6c582eb251356e9d3f9381
DonovanDebenPXL <donovan.deben@student.pxl.be>
2025-11-21 12:01:01 +0100

    Oef Fibonacci afgemaakt

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Fibonacci
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacciNumbers = new int[12];
+            fibonacciNumbers[0] = 0;
+            fibonacciNumbers[1] = 1;
+            for (int i = 2; i < fibonacciNumbers.Length; i++)
+            {
+                fibonacciNumbers[i] = fibonacciNumbers[i - 2] + fibonacciNumbers[i - 1];
+            }
+
+            foreach (int number in fibonacciNumbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
